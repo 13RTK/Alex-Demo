e731a92aca3fc4152a05ccad4be56c8637dca763
alex <907785012@qq.com>
2021-05-06 13:41:30 +0800

    2021.5.6 update by Alex

---
+++ JDBCdemo/src/day04/Demo.java
@@ -0,0 +1,12 @@
+package day04;
+import com.google.util.Menu;
+
+public class Demo {
+    public static void main(String[] args) throws Exception{
+        run();
+    }
+
+    public static void run() throws Exception{
+        Menu.welcome();
+    }
+}
